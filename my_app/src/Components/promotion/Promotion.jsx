d5bc02e3256713279ba79d447db9be0593ac3643
VincentMugondora <vinmugondora@gmail.com>
2024-07-31 09:22:00 +0200

    added  promotion page

--- my_app/src/Components/promotion/Promotion.jsx
+++ my_app/src/Components/promotion/Promotion.jsx
@@ -8,8 +8,18 @@ const Promotion = () => {
         "../../assets/images/bedroom-with-bed-chair-front-sliding-glass-door.jpg",
       text: "lorem100",
     },
-  ];
-  const blogs = [
+    {
+      name: "GET THE BEST DEALS WITH TIMMERMAN INDUSTRIES",
+      image:
+        "../../assets/images/bedroom-with-bed-chair-front-sliding-glass-door.jpg",
+      text: "lorem100",
+    },
+    {
+      name: "GET THE BEST DEALS WITH TIMMERMAN INDUSTRIES",
+      image:
+        "../../assets/images/bedroom-with-bed-chair-front-sliding-glass-door.jpg",
+      text: "lorem100",
+    },
     {
       name: "GET THE BEST DEALS WITH TIMMERMAN INDUSTRIES",
       image:
@@ -17,36 +27,18 @@ const Promotion = () => {
       text: "lorem100",
     },
   ];
-  const blogs = [
-    {
-      name: "GET THE BEST DEALS WITH TIMMERMAN INDUSTRIES",
-      image:
-        "../../assets/images/bedroom-with-bed-chair-front-sliding-glass-door.jpg",
-      text: "lorem100",
-    },
-  ];
-  const blogs = [
-    {
-      name: "GET THE BEST DEALS WITH TIMMERMAN INDUSTRIES",
-      image:
-        "../../assets/images/bedroom-with-bed-chair-front-sliding-glass-door.jpg",
-      text: "lorem100",
-    },
-  ];
+ 
 
   return (
     <>
-      <div className="promotion">
-        <h1>GET THE BEST DEALS WITH TIMMERMAN INDUSTRIES</h1>
-        <div className="promo3">
-          <h2>Win A trip to an exotic place all expenses paid</h2>
-        </div>
-        <div className="promo3">
-          <h2>Win A trip to an exotic place all expenses paid</h2>
-        </div>
-        <div className="promo3">
-          <h2>Win A trip to an exotic place all expenses paid</h2>
-        </div>
+      <div className="blog-cont" key={Math.random()}>
+        {blogs.map((blog)=>{
+          <div className="blog">
+            <div className="blog-image">
+              <img src={blog.image} alt={blog.name} />
+              </div>
+          </div>
+        })}
       </div>
     </>
   );
